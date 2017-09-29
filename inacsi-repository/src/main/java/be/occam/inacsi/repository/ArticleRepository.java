b0e96eb7e566f552726050964dac3e6b8f31f790
Sven Gladines <sven.gladines@gmail.com>
2017-09-29 15:40:57 +0200

    move from backend to enable class enhancing

---
+++ inacsi-repository/src/main/java/be/occam/inacsi/repository/ArticleRepository.java
@@ -0,0 +1,16 @@
+package be.occam.inacsi.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface ArticleRepository extends JpaRepository<ArticleEntity, String>{
+	
+	public ArticleEntity findOneByUuid( String uuid );
+	public ArticleEntity findOneByIdAndVersion( String id, String version );
+	
+	public List<ArticleEntity> findByPage( String page );
+
+}
