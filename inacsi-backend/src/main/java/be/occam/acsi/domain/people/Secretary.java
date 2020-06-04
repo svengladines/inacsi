b427b08ca41d852ed8d11ee748eb3fe623e8cf36
Sven Gladines <sven.gladines@gmail.com>
2020-06-04 23:19:21 +0200

    release 4/6/2020

--- inacsi-backend/src/main/java/be/occam/acsi/domain/people/Secretary.java
+++ inacsi-backend/src/main/java/be/occam/acsi/domain/people/Secretary.java
@@ -15,9 +15,11 @@ public class Secretary {
 		this.therapists = map();
 		
 		this.therapists.put("jacobs", new Therapist("Laura", "Jacobs") );
+		this.therapists.put("jacops", new Therapist("Sigrid", "Jacops") );
 		this.therapists.put("nys", new Therapist("Charlotte", "Nys") );
 		this.therapists.put("poel", new Therapist("Sabine", "Poel" ) );
 		this.therapists.put("verbiest", new Therapist("Siel", "Verbiest" ) );
+		this.therapists.put("vanhauwaert", new Therapist("Ellen", "Van Hauwaert" ) );
 		this.therapists.put("vuerinckx", new Therapist("Katrien", "Vuerinckx" ) );
 		
 	}
